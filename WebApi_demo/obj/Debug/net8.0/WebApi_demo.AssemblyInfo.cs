07ba962c68f630d464566d76fa369c989a181197
Azamat Kopzhasharov <kopzha01@gmail.com>
2024-05-29 02:23:28 +0600

    remove unnecessary files

--- WebApi_demo/obj/Debug/net8.0/WebApi_demo.AssemblyInfo.cs
+++ WebApi_demo/obj/Debug/net8.0/WebApi_demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi_demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c43e70d09798237a404e48dfe98e4017ce77facc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi_demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi_demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
